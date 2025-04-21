59583284c66ac68cf896283c563a46059bcc26bf
AmteurPotion <1k0j2i4@gmail.com>
2025-04-22 08:13:12 +0900

    Cleanup
    * 디버깅용 Console.WriteLine 제거
    * 콘솔 출력을 다중행 문자열로 변경
    * linq sql 구문을 이용한 jobNames 생성

--- Core/CreatePlayer.cs
+++ Core/CreatePlayer.cs
@@ -1,121 +1,115 @@
-﻿using Starfall.Contents.Binary;
+﻿using Spectre.Console;
+using Starfall.Contents.Binary;
 using Starfall.IO;
 using Starfall.IO.CUI;
 
 namespace Starfall.Core
 {
-    public static class CreatePlayer
+  public static class CreatePlayer
+  {
+    #region Job Name
+    public enum JobName
     {
-        #region Job Name
-        public enum JobName
-        {
-            None = 0,
-            Warrior = 1,
-            Wizard = 2,
-            Rogue = 3,
-        }
-
-        /// <summary>
-        /// ex) JobName.None.GetJobNameToKor()
-        /// </summary>
-        /// <param name="className"></param>
-        /// <returns></returns>
-        public static string GetJobNameToKor(this JobName className) => className switch
-        {
-            JobName.None => "없음",
-            JobName.Warrior => "전사",
-            JobName.Wizard => "마법사",
-            JobName.Rogue => "도적",
-            _ => "",
-        };
-        #endregion
-
-        public static void CreateNewPlayer()
-        {
-            SetPlayerName();
-        }
-
-        static void SetPlayerName()
-        {
-            // ===========================
-            Console.Clear();
-            ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt", ConsoleColor.DarkMagenta, ConsoleColor.Green);
-            Console.WriteLine();
-            // ===========================
-
-            // 이름 설정
-            Console.WriteLine(
-                "\n" +
-                "원하시는 이름을 입력해주세요.");
-
-            Console.WriteLine();
-            string input = Console.ReadLine() ?? "";
-
-            Console.WriteLine(
-                "\n" +
-                $"입력하신 이름은 [{input}]입니다." +
-                "\n");
-
-            string name = input;
-
-            // ===========================
-            // 메뉴 리스트 설정
-            switch (MenuUtil.OpenMenu("저장", "다시 입력", "메인 메뉴로"))
-            {
-                case 0:
-                    // 저장
-                    //Console.WriteLine($"{name}");
-                    StorageController.SetSaveName($"{name}");
-                    var data = new GameData();
-                    data.Name = name;
-                    SetPlayerJob(data);
-                    break;
-                case 1:
-                    SetPlayerName();
-                    break;
-                case 2:
-                case -1: GameManager.EnterMain(); break;
-            }
-            // ===========================
-        }
-
-        static void SetPlayerJob(GameData data)
-        {
-            // ===========================
-            //Console.Clear();
-            ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt", ConsoleColor.DarkMagenta, ConsoleColor.Green);
-            Console.WriteLine();
-            // ===========================
-
-            // 메뉴 리스트 설정
-            var selections = new List<string>();
-
-            foreach (JobName jobName in Enum.GetValues(typeof(JobName)))
-            {
-                if (jobName == JobName.None) continue;
-
-                selections.Add(jobName.GetJobNameToKor());
-            }
-
-            // ===========================
-            // 메뉴 리스트 생성
-            string[] jobNameList = new string[selections.Count];
-
-            for (int i = 0; i < selections.Count; i++)
-            {
-                jobNameList[i] = selections[i];
-            }
-
-            int selection = MenuUtil.OpenMenu(jobNameList);
-
-            if (selection < 0) { SetPlayerName(); }
-            else
-            {
-                data.Job = (JobName)selection;
-                // 메뉴 씬으로 이동
-                GameManager.StartGame(data);
-            }
-            // ===========================
-        }
+      None = 0,
+      Warrior = 1,
+      Wizard = 2,
+      Rogue = 3,
     }
+
+    /// <summary>
+    /// ex) JobName.None.GetJobNameToKor()
+    /// </summary>
+    /// <param name="className"></param>
+    /// <returns></returns>
+    public static string GetJobNameToKor(this JobName className) => className switch
+    {
+      JobName.None => "없음",
+      JobName.Warrior => "전사",
+      JobName.Wizard => "마법사",
+      JobName.Rogue => "도적",
+      _ => "",
+    };
+    #endregion
+
+    public static void CreateNewPlayer()
+    {
+      SetPlayerName();
+    }
+
+    private static void SetPlayerName()
+    {
+      // ===========================
+      Console.Clear();
+      ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt");
+      Console.WriteLine();
+      // ===========================
+
+      // 이름 설정
+      AnsiConsole.MarkupLine("""
+
+      원하시는 이름을 입력해주세요.
+      """);
+
+      Console.WriteLine();
+      string input = Console.ReadLine() ?? "";
+
+      AnsiConsole.MarkupLine("""
+            
+      입력하신 이름은 [{input}]입니다.
+
+      """);
+
+      string name = input;
+
+      // ===========================
+      // 메뉴 리스트 설정
+      switch (MenuUtil.OpenMenu("저장", "다시 입력", "메인 메뉴로"))
+      {
+        case 0:
+          // 저장
+          StorageController.SetSaveName($"{name}");
+          var data = new GameData
+          {
+            Name = name
+          };
+          SetPlayerJob(data);
+          break;
+
+        case 1:
+          SetPlayerName();
+          break;
+
+        case 2:
+        case -1: GameManager.EnterMain(); break;
+      }
+      // ===========================
+    }
+
+    private static void SetPlayerJob(GameData data)
+    {
+      // ===========================
+      Console.Clear();
+      ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt");
+      Console.WriteLine();
+      // ===========================
+
+      // 메뉴 리스트 Linq 구문 이용해서 압축
+      var jobNames = (
+          from key in Enum.GetValues<JobName>()
+          where key != JobName.None
+          select key.GetJobNameToKor()
+      ).ToArray();
+
+      int selection = MenuUtil.OpenMenu(jobNames);
+
+      if (selection < 0) SetPlayerName();
+      else
+      {
+        data.Job = (JobName)(selection + 1);
+        // 메뉴 씬으로 이동
+        GameManager.StartGame(data);
+      }
+      // ===========================
+    }
+  }
 }
