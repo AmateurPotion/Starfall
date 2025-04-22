0bd0c8fe90ecc825a2efa0e7643bcda63e982aef
unity9Parkjaehyun <game_samson@naver.com>
2025-04-22 11:17:45 +0900

    JSON 파일, 매니저 구현
    - D:\Starfall\Resources\monster
     Minion,SiegeMinion,Volding
       Json 파일 생성
    
    -D:\Starfall GameManager.cs에
     foreach DirectoryInfo 경로지정
    - gamedata using문 io 수정

--- GameManager.cs
+++ GameManager.cs
@@ -6,98 +6,120 @@ using Starfall.Core;
 using Starfall.IO;
 using Starfall.IO.CUI;
 
+
 namespace Starfall
 {
-  public static class GameManager
-  {
-    public static bool Loaded { get; private set; } = false;
-    public static readonly Dictionary<string, Item> items = [];
-    public static void Init()
+    public static class GameManager
     {
-      if (Loaded) return;
-      StorageController.Init();
-      Console.Title = "StarFall";
-
-      foreach (var info in new DirectoryInfo("./Resources/items/").GetFiles())
-      {
-        try
+        public static bool Loaded { get; private set; } = false;
+        public static readonly Dictionary<string, Item> items = [];
+        public static readonly Dictionary<string, MonsterData> monsters = [];
+        public static void Init()
         {
-          if (info.Name.Contains(".json"))
-          {
-            var name = info.Name.Replace(".json", "");
-            var stream = info.OpenRead();
-            var data = JsonSerializer.Deserialize<Item>(stream);
+            if (Loaded) return;
+            StorageController.Init();
+            Console.Title = "StarFall";
 
-            items[name] = data;
+            foreach (var info in new DirectoryInfo("./Resources/item/").GetFiles())
+            {
+                try
+                {
+                    if (info.Name.Contains(".json"))
+                    {
+                        var name = info.Name.Replace(".json", "");
+                        var stream = info.OpenRead();
+                        var data = JsonSerializer.Deserialize<Item>(stream);
 
-            stream.Close();
-          }
+                        items[name] = data;
+
+                        stream.Close();
+                    }
+                }
+                catch (JsonException) { }
+
+            }
+            // foreach 문 넣었음   by. 박재현 
+            foreach (var info in new DirectoryInfo("./Resources/monster/").GetFiles())
+            {
+                try
+                {
+                    if (info.Name.Contains(".json"))
+                    {
+                        var name = info.Name.Replace(".json", "");
+                        var stream = info.OpenRead();
+                        var data = JsonSerializer.Deserialize<MonsterData>(stream);
+
+                        monsters[name] = data;
+
+                        stream.Close();
+                    }
+                }
+                catch (JsonException) { }
+            }
+
+            Loaded = true;
         }
-        catch (JsonException)
-        { }
-      }
 
-      Loaded = true;
+
+
+        public static Game StartGame(GameData data)
+        {
+            var game = new Game(data);
+            game.Start();
+
+            return game;
+        }
+
+        public static void JoinGame()
+        {
+
+        }
+
+        // 추가 by. 최영임 
+        // Program.cs에서 가져옴. 
+        public static void EnterMain()
+        {
+        Menu:
+            Console.Clear();
+            ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt");
+            Console.WriteLine();
+
+            StorageController.SetSaveName("default");
+            switch (MenuUtil.OpenMenu("새로운 여정", "데이터 불러오기", "다른 여정 참여"))
+            {
+                case 0:
+                    // 새로운 여정 - 새 게임
+                    // 추가 by. 최영임 
+                    // 수정 by. 최영임 
+                    // 플레이어 첫 생성
+                    CreatePlayer.CreateNewPlayer();
+                    break;
+
+                case 1:
+                    // 데이터 불러오기 - 데이터 불러오기
+                    Console.Clear();
+                    var menu = (from path in StorageController.GetSaveNames() select path.Replace("./saves/world\\", "")).ToArray();
+
+                    AnsiConsole.MarkupLine("불러올 데이터를 선택하세요. \n");
+                    var select = MenuUtil.OpenMenu([.. menu, "\n돌아가기"]);
+
+                    if (select > -1 && select < menu.Length)
+                    {
+                        StorageController.SetSaveName(menu[select]);
+                        GameManager.StartGame(StorageController.LoadBinary<GameData>("data"));
+                    }
+                    else goto Menu;
+                    break;
+
+                case 2:
+                    // 다른 여정 참여 - 다른 여정 참여
+                    GameManager.JoinGame();
+                    Console.WriteLine("개발중입니다.");
+                    Console.ReadKey();
+                    goto Menu;
+
+                case -1: goto Menu;
+            }
+        }
     }
-
-    public static Game StartGame(GameData data)
-    {
-      var game = new Game(data);
-      game.Start();
-
-      return game;
-    }
-
-    public static void JoinGame()
-    {
-
-    }
-
-    // 추가 by. 최영임 
-    // Program.cs에서 가져옴. 
-    public static void EnterMain()
-    {
-    Menu:
-      Console.Clear();
-      ConsoleUtil.PrintTextFile("Starfall.Resources.intro.txt");
-      Console.WriteLine();
-
-      StorageController.SetSaveName("default");
-      switch (MenuUtil.OpenMenu("새로운 여정", "데이터 불러오기", "다른 여정 참여"))
-      {
-        case 0:
-          // 새로운 여정 - 새 게임
-          // 추가 by. 최영임 
-          // 수정 by. 최영임 
-          // 플레이어 첫 생성
-          CreatePlayer.CreateNewPlayer();
-          break;
-
-        case 1:
-          // 데이터 불러오기 - 데이터 불러오기
-          Console.Clear();
-          var menu = (from path in StorageController.GetSaveNames() select path.Replace("./saves/world\\", "")).ToArray();
-
-          AnsiConsole.MarkupLine("불러올 데이터를 선택하세요. \n");
-          var select = MenuUtil.OpenMenu([.. menu, "\n돌아가기"]);
-
-          if (select > -1 && select < menu.Length)
-          {
-            StorageController.SetSaveName(menu[select]);
-            GameManager.StartGame(StorageController.LoadBinary<GameData>("data"));
-          }
-          else goto Menu;
-          break;
-
-        case 2:
-          // 다른 여정 참여 - 다른 여정 참여
-          GameManager.JoinGame();
-          Console.WriteLine("개발중입니다.");
-          Console.ReadKey();
-          goto Menu;
-
-        case -1: goto Menu;
-      }
-    }
-  }
 }
