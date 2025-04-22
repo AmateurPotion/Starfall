d968a83d5c41a830104b3a6300447415fb5c0244
AmteurPotion <1k0j2i4@gmail.com>
2025-04-23 02:35:12 +0900

    기초 연산자 오버로딩 추가

--- Utils/Vector2Int.cs
+++ Utils/Vector2Int.cs
@@ -23,6 +23,12 @@ namespace Starfall.Utils
       y = this.y;
     }
 
+    public static Vector2Int operator +(Vector2Int origin, Vector2Int vector)
+      => new(origin.x + vector.x, origin.y + vector.y);
+
+    public static Vector2Int operator +(Vector2Int origin, int value)
+      => new(origin.x + value, origin.y + value);
+
     public static readonly Vector2Int Up = new(0, 1);
     public static readonly Vector2Int Down = new(0, -1);
     public static readonly Vector2Int Left = new(-1, 0);
