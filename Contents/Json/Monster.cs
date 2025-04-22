5dbe0b7fa9a638a6e646c4cd494078da6853a74c
AmteurPotion <1k0j2i4@gmail.com>
2025-04-22 09:15:11 +0900

    몬스터 구현을 위한 클래스 분리 구현

--- Contents/Json/Monster.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Starfall.IO.Contents.Json
-{
-    public struct Monster
-    {
-        public int Hp { get; set; }
-        public int Atk { get; set; }
-        public int Level { get; set; }
-    }
-}
---
+++ Contents/Json/MonsterData.cs
@@ -0,0 +1,9 @@
+﻿namespace Starfall.IO.Contents.Json;
+
+public struct MonsterData()
+{
+  public string Name { get; set; } = "MonsterName";
+  public int Hp { get; set; } = 10;
+  public int Atk { get; set; } = 3;
+  public int Level { get; set; } = 1;
+}
