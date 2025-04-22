34411265d8ab01e5dcc00c806292859b316947df
AmteurPotion <1k0j2i4@gmail.com>
2025-04-23 02:18:51 +0900

    던전 구현하기 드럽게 으렵네 지짜....

--- Contents/Floor.cs
+++ Contents/Floor.cs
@@ -4,6 +4,7 @@ namespace Starfall.Contents;
 
 public class Floor
 {
+  private static readonly Random random = new();
   public readonly int length;
   public List<StageNode>[] data;
   public Vector2Int current = new();
@@ -23,36 +24,59 @@ public class Floor
       var line = data[y] = [];
 
       // 기초 스테이지 생성
-      for (int x = 0; x < length; x++)
+      for (int x = 0, l = this.length; x < l; x++)
       {
         line.Add(x switch
         {
           // 시작 노드
           0 => new() { type = StageType.Start },
           // 보스 노드
-          var i when i == length - 2 => lastShopNode,
-          var i when i == length - 1 => bossNode,
-          // 무작위 노드 생성
-          _ => new()
-          {
-            type = StageType.Battle,
-            middle = true,
-            top = y != 0,
-            bottom = y != data.Length - 1
-          }
+          var i when i == l - 2 => lastShopNode,
+          var i when i == l - 1 => bossNode,
+          // 기본 전투 생성
+          _ => new() { type = StageType.Battle }
         });
       }
-
-      // 일부 노드 삭제하기
-      int deleteAmount = 3;
-      for (int x = 0; x < length && deleteAmount > 0; x++)
-      {
-
-      }
-
-      //  이벤트 & 상점 스테이지 삽입
-      int eventAmount = 3, shopAmount = 3;
     }
+
+    // 각 라인마다 1개 노드 삭제하기
+    for (int x = 0; x < length; x++)
+    {
+      data[random.NextInt64(0, height)][x + 1].type = StageType.None;
+    }
+
+    //  이벤트 & 상점 스테이지 삽입
+    int eventAmount = length < 3 ? length : 3,
+     shopAmount = length < 2 ? length : 2;
+
+    var eventList =
+      (from n in Enumerable.Range(1, length)
+       orderby random.Next()
+       select n).Take(eventAmount);
+
+    foreach (var n in eventList)
+    {
+      (from line in data
+       where line[n].type == StageType.Battle
+       orderby random.Next()
+       select line[n]).GetEnumerator().Current.type = StageType.Event;
+    }
+
+    var shopList =
+      (from n in Enumerable.Range(1, length)
+       orderby random.Next()
+       select n).Take(shopAmount);
+
+    foreach (var n in shopList)
+    {
+      (from line in data
+       where line[n].type == StageType.Battle
+       orderby random.Next()
+       select line[n]).GetEnumerator().Current.type = StageType.Shop;
+    }
+
+    // 경로 생성
+
 
     #endregion
   }
@@ -60,9 +84,11 @@ public class Floor
   public void Render()
   {
     Console.Clear();
+    var (sx, sy) = Console.GetCursorPosition();
 
     for (int x = 0; x < length; x++)
     {
+
     }
   }
 }
