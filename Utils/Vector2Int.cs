97ea0afc0a3a8d7504e59c8aba3fc3ab847b7498
AmteurPotion <1k0j2i4@gmail.com>
2025-04-23 02:26:46 +0900

    정적 방향 벡터 추가

--- Utils/Vector2Int.cs
+++ Utils/Vector2Int.cs
@@ -17,10 +17,15 @@ namespace Starfall.Utils
       this.y = y;
     }
 
-    public void Deconstruct(out int x, out int y)
+    public readonly void Deconstruct(out int x, out int y)
     {
       x = this.x;
       y = this.y;
     }
+
+    public static readonly Vector2Int Up = new(0, 1);
+    public static readonly Vector2Int Down = new(0, -1);
+    public static readonly Vector2Int Left = new(-1, 0);
+    public static readonly Vector2Int Right = new(1, 0);
   }
 }
