c5db8e5083cb683c69e58459ac82381551286d69
Youngim <immy0815@users.noreply.github.com>
2025-04-25 19:15:08 +0900

    QuestSystem_TempUpdate

--- Core/Quest/QuestGoal.cs
+++ Core/Quest/QuestGoal.cs
@@ -6,6 +6,8 @@ namespace Starfall.Core.Quest
 		public string Target { get; private set; }
 		public int Amount { get; private set; }
 
+		string questType;
+
 		public QuestGoal(string goalStr)
 		{
 			// 퀘스트 설정 할 때 (예를 들어: 던전)
@@ -16,8 +18,45 @@ namespace Starfall.Core.Quest
 
 			var targetAmount = parts[1].Split('*');
 			Target = targetAmount[0];
-			Amount = targetAmount.Length > 1 ? int.Parse(targetAmount[1]) : 1;
+			Amount = targetAmount.Length > 1 ? int.Parse(targetAmount[1]) : -1;
+		}
+
+		// 예: 미니언(target) 5(Amount)마리 처치(type) (2(condition.CurrentCount)/5(amount))
+		public string GoalToString(int curCount)
+		{
+			string goalTxt = $"{Target} {GetAmountToString()} {QuestTypeClass.GetTypeToKor(Type)} ({curCount}/{Amount})";
+
+			return goalTxt;
+		}
+
+		public string GoalToString()
+		{
+			string goalTxt = $"{Target} {GetAmountToString()} {QuestTypeClass.GetTypeToKor(Type)}";
+
+			return goalTxt;
+		}
+
+		public string GetAmountToString()
+		{
+			if (Amount != -1)
+			{
+				return $"{Amount}{GetAmountUnit()}";
+			}
+			else
+				return "";
+		}
+
+		// 표시 단위
+		string GetAmountUnit()
+		{
+			return Type switch
+			{
+				QuestType.Kill => "마리",
+				QuestType.LevelUp or
+				QuestType.StatUp => "만큼",
+				QuestType.Equip or    
+				_ => ""                           
+			};
 		}
 	}
-
 }
--- Core/Quest/QuestType.cs
+++ Core/Quest/QuestType.cs
@@ -7,4 +7,16 @@ namespace Starfall.Core.Quest
 		LevelUp,
 		StatUp
 	}
+
+	static class QuestTypeClass
+	{
+		public static string GetTypeToKor(QuestType type) => type switch
+		{
+			QuestType.Kill => "처치",
+			QuestType.Equip => "장착",
+			QuestType.LevelUp => "레벨 업",
+			QuestType.StatUp => "스탯 업",
+			_ => "",
+		};
+	}
 }
